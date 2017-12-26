f2d765130eeb45f594b1439f7442115021f7decb
jiangq <jiangq.leaves@gmail.com>
2017-12-26 17:39:11 +0800

    change config

--- config/config.default.js
+++ config/config.default.js
@@ -1,27 +1,73 @@
 'use strict';
-const thrift = require('thrift');
-const transport = thrift.TBufferedTransport;
-const protocol = thrift.TBinaryProtocol;
+const thrift = require('n-thrift');
+const TBufferedTransport = thrift.TBufferedTransport;
+const TBinaryProtocol = thrift.TBinaryProtocol;
 /**
  * egg-thrift default config
  * @member Config#thrift
  * @property {String} SOME_KEY - some description
  */
 exports.thrift = {
-  type: {},
-  connectType: 'http',
-  defaultClient: {
-    transport,
-    protocol,
-    options: {},
+  package: null,
+  dir: 'thrift',
+  compilerOptions: {},
+  thriftOptions: {
+    timeout: 5000,
   },
-  clients: {
+  defaultClientConfig: {
+    multiplexed: false,
+    serviceName: '',
+    transport: TBufferedTransport,
+    protocol: TBinaryProtocol,
+    connectType: 'tcp',
+    host: '127.0.0.1',
+    path: '', // only works when connection type is http
+    port: '80',
+    headers: {
+    },
   },
-  defaultService: {
-    transport,
-    protocol,
+  defaultServiceConfig: {
+    multiplexed: false,
+    serviceName: '',
+    transport: TBufferedTransport,
+    protocol: TBinaryProtocol,
+    connectType: 'tcp',
+    host: '127.0.0.1',
+    path: '',  // only works when connection type is http
+    port: '80',
+    headers: {
+    },
   },
-  services: {
-
-  },
+  clients: [
+    // {
+    //   client: '',
+    //   alias: '',
+    //   multiplexed: false,
+    //   serviceName: '',
+    //   transport: TBufferedTransport,
+    //   protocol: TBinaryProtocol,
+    //   connectType: 'tcp',
+    //   host: '127.0.0.1',
+    //   path: '', only works when connection type is http
+    //   port: '80',
+    //   headers: {
+    //   },
+    // },
+  ],
+  services: [
+    // {
+    //   service: '',
+    //   processor: '',
+    //   multiplexed: false,
+    //   serviceName: '',
+    //   transport: TBufferedTransport,
+    //   protocol: TBinaryProtocol,
+    //   connectType: 'tcp',
+    //   host: '127.0.0.1',
+    //   path: '',  only works when connection type is http
+    //   port: '80',
+    //   headers: {
+    //   },
+    // },
+  ],
 };
--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,59 @@
 import { EggApplication } from 'egg';
+import { TTransport, TProtocol } from 'thrift';
+
+export interface ClientConfig {
+  client: string;
+  alias?: string;
+  multiplexed?: boolean;
+  serviceName?: string;
+  transport: TTransport;
+  protocol: TProtocol;
+  connectType: 'http' | 'https' | 'tcp' | 'tls';
+  host: string;
+  path?: string;
+  port?: string;
+  headers?: {
+    [name: string]: number | string | string[] | undefined;
+  };
+}
+
+export interface ServiceConfig {
+  service: string;
+  processor: string;
+  multiplexed?: boolean;
+  serviceName?: string;
+  transport: TTransport;
+  protocol: TProtocol;
+  connectType: 'http' | 'https' | 'tcp' | 'tls';
+  host: string;
+  path?: string;
+  port?: string;
+  headers?: {
+    [name: string]: number | string | string[] | undefined;
+  };
+}
+
+export interface ThriftConfig {
+  package?: any;
+  dir?: string;
+  compilerOptions: any;
+  thriftOptions: {
+    timeout: number;
+  };
+  defaultClientConfig: ClientConfig;
+  defaultServiceConfig: ServiceConfig;
+  clients: Array<ClientConfig>;
+  services: Array<ServiceConfig>;
+}
 
 declare module 'egg' {
-  export interface IThriftClient { }
-  export interface IThriftType { }
+  export interface IThriftClient {}
+  export interface IThriftType {}
   export interface Application extends EggApplication {
-    thrift: IThriftClient,
-    thriftType: IThriftType,
+    thrift: IThriftClient;
+    thriftType: IThriftType;
+  }
+  export interface EggAppConfig {
+    thrift: ThriftConfig;
   }
 }
