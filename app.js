dc4b5c6a5910751bef0394b6a49cf47346239d0d
jiangq <jiangq.leaves@gmail.com>
2017-12-27 15:57:02 +0800

    remove creator

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict';
-const createHttpClient = require('./lib/create_http_client');
-const createThriftType = require('./lib/create_thrift_type');
-/**
- *
- * @param {Egg.Application} app
- */
+
 module.exports = app => {
-  app.config.coreMiddleware.push('thriftHandler');
-  createHttpClient(app);
-  createThriftType(app);
+  app.config.coreMiddleware.push('thriftServiceHandler');
 };
