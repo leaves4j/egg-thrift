d7ab6d3655bfb9fc7f1ad3112a51ae111c4794cc
jiangq <jiangq.leaves@gmail.com>
2017-12-28 11:17:13 +0800

    add watcher and compiler

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
-
+const thriftCompiler = require('./lib/compiler');
+const createThriftWatcher = require('./lib/watcher');
 module.exports = app => {
+  app.beforeStart(async () => {
+    await thriftCompiler(app);
+  });
+  createThriftWatcher(app);
   app.config.coreMiddleware.push('thriftServiceHandler');
 };
